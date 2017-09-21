cf3d7d6c700b03006907d6ad8661cdb62426c727
Gleb Karapish <gkarapish@bloomberg.net>
2017-09-21 15:58:05 -0400

    Update BFS, BST, ifaceVsAbstract

--- BST.cpp
+++ BST.cpp
@@ -1,69 +1,49 @@
 #include <iostream>
-#include <queue>
 
 using namespace std;
 
-template <typename T=size_t>
-struct Node {
-    T v;
-    Node* left;
-    Node* right;
-    
-    static Node<T>* create(T v, Node<T>* l, Node<T>* r) {
-        auto n = new Node;
-        n->v = v;
-        n->left = l;
-        n->right = r;
-        return n;
-    }
+struct node
+{
+   node(int v, node* l = nullptr, node* r = nullptr) :
+      v {v}, l {l}, r {r}
+   { }
+
+   int v;
+   node* l;
+   node* r;
 };
 
-int main()
+
+bool search( node* n, int value )
 {
-    Node<int>* t = 
-    Node<int>::create(1, 
-        Node<int>::create(2, 
-            Node<int>::create(3,nullptr, nullptr),
-            Node<int>::create(4,nullptr, nullptr)
-        ),
-        Node<int>::create(5, 
-            Node<int>::create(3,nullptr, nullptr),
-            Node<int>::create(4,nullptr, nullptr)
-        )
-    );
-    
-    struct NodeL {
-        Node<int>* node;
-        size_t level;
-        
-        static NodeL* create(Node<int>* n, size_t l) {
-            auto node = new NodeL;
-            node->node = n;
-            node->level = l;
-            return node;
-        }
-    };
-    
-    queue<NodeL*> q;
-    q.push(NodeL::create(t, 1));
-    auto level = 1;
-    
-    while(!q.empty()) {
-        auto i = q.front();
-        if(i->level != level) {
-            cout << endl;
-            ++level;
-            continue;
-        }
-        
-        q.pop();
-        cout << i->node->v << " ";
-        
-        if(i->node->left)
-            q.push(NodeL::create(i->node->left, level+1));
-        if(i->node->right)
-            q.push(NodeL::create(i->node->right, level+1));
-    }
-    
-    return 0;
+   if(!n) 
+      return false;
+
+   if(n->v == value)
+      return true;
+
+   cout << "visiting: " << n->v << endl;
+
+   bool found = false;
+   if(n->l)
+      found = found || search(n->l, value);
+
+   if(!found && n->r)
+      found = found || search(n->r, value);
+
+   return found;
+}
+
+auto main() -> void {
+   node n(1, 
+      &node(2, 
+         &node(3),
+         &node(4)
+      ),
+      &node(5)
+   );
+
+   cout << "found? " << search(&n, 5) << endl;
+   int i;
+   cin >> i;
 }
